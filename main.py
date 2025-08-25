64ad2b5ce0d3b903cc54cec85948980e2718423f
SCurry <61257658+Born-A-Bot@users.noreply.github.com>
2025-08-25 16:14:55 -0400

    plot data

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import yfinance as yf
 
 #prompt user for share name
-CHK_STK = input("Enter stock name: ")
-
+chk_stock = input("Enter stock name: ")
 
 #fetch historical market data
-stock_data = yf.Ticker(CHK_STK).history(period="5d")
+stock_data = yf.Ticker(chk_stock).history(period="5d")
 
-#extract market price for last 7 days
-final_market_price = stock_data["Close"].iloc[-5]
+#extract market price for last 7 days = track start and close for 5 days???
+final_market_prices = stock_data["Close"]
 
-#display 5 day close price
-print("Stock Data:", final_market_price)
-
+#display 5 day close pric
+print("Stock Data:", final_market_prices)
 
 #option to check additional stocks
-CHK_AGAIN = input("Check another stock? Y or N: ")
-if CHK_AGAIN == "Y":
-    print(CHK_STK = input("Enter stock share name: "))
+chk_another = input("Check another stock? Y or N: ")
+if chk_another == "Y":
+    print(chk_stock = input("Enter stock share name: "))
 else:
     print("Thanks!")
 
