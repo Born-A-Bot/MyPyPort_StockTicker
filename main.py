6df0b2160a469a16f4922c995c1c62b71f1b978b
SCurry <61257658+Born-A-Bot@users.noreply.github.com>
2025-09-26 21:02:19 -0400

    QoQ -
    quarter-over-quarter collection and graphing

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
+
 import yfinance as yf
+import matplotlib.pyplot as plt
 
 #prompt user for share name
 chk_stock = input("Enter stock name: ")
 
-#fetch historical market data
+#fetch historical market data for past 5 days
 stock_data = yf.Ticker(chk_stock).history(period="5d")
 
-#extract market price for last 7 days = track start and close for 5 days???
+#extract close price
 final_market_prices = stock_data["Close"]
 
-#display 5 day close pric
-print("Stock Data:", final_market_prices)
+#display 5 day close price
+print("Stock Close Prices:", final_market_prices)
 
-#option to check additional stocks
-chk_another = input("Check another stock? Y or N: ")
-if chk_another == "Y":
-    print(chk_stock = input("Enter stock share name: "))
-else:
-    print("Thanks!")
+#plot the data
+fig, ax = plt.subplots()
+ax.plot(final_market_prices)
+ax.set_title("Five Day Stock Data")
+ax.set_ylabel('Close Price - U.S. Dollars ($)')
+ax.set_xlabel('Date')
+plt.show()
+
 
